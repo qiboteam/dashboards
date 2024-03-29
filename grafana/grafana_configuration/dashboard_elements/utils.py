51bc99f853eab0b9da42ff6aaa0d5494f3274431
GabrielePalazzo <73099233+GabrielePalazzo@users.noreply.github.com>
2024-03-29 13:47:49 +0400

    refactor: Moved default GridPos values to constants

--- grafana/grafana_configuration/dashboard_elements/utils.py
+++ grafana/grafana_configuration/dashboard_elements/utils.py
@@ -1,9 +1,16 @@
 from dataclasses import dataclass
 
+DEFAULT_WIDTH = 24
+DEFAULT_HEIGHT = 1
+
 
 @dataclass
 class GridPos:
     x: int
+    """Top left x position of the dashboard element."""
     y: int
-    w: int = 24
-    h: int = 1
+    """Top left y position of the dashboard element."""
+    w: int = DEFAULT_WIDTH
+    """Width of the dashboard element (maximum is 24)."""
+    h: int = DEFAULT_HEIGHT
+    """Height of the dashboard element."""
