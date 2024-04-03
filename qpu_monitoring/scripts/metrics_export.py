86b8aaaf8964865ee9243e38cc8a5caba6ac5b76
GabrielePalazzo <73099233+GabrielePalazzo@users.noreply.github.com>
2024-04-03 11:13:01 +0400

    refactor: Give platform name to metrics

--- qpu_monitoring/scripts/metrics_export.py
+++ qpu_monitoring/scripts/metrics_export.py
@@ -3,16 +3,16 @@
 import json
 from pathlib import Path
 
+import yaml
 from prometheus_client import CollectorRegistry, Gauge, push_to_gateway
 from qibocal.auto.serialize import deserialize
 
 
-def from_path(json_path):
+def from_path(json_path: Path):
     return json.loads(json_path.read_text())
 
 
-def get_data():
-    qibocal_output_folder = Path(__file__).parent.parent / "report"
+def get_data(qibocal_output_folder: Path):
     for n in range(1):
         path_t1 = deserialize(
             from_path(qibocal_output_folder / "data" / f"t1_{n}" / "results.json")
@@ -35,8 +35,12 @@ def get_data():
 
 
 if __name__ == "__main__":
+    qibocal_output_folder = Path(__file__).parent.parent / "report"
+    platform = yaml.safe_load((qibocal_output_folder / "runcard.yml").read_text())[
+        "platform"
+    ]
     registry = CollectorRegistry()
-    t1 = Gauge(f"t1", f"T1 in us", registry=registry)
-    t2 = Gauge(f"t2", f"T2 in us", registry=registry)
-    fidelity = Gauge(f"assignment_fidelity", f"T2 in us", registry=registry)
-    get_data()
+    t1 = Gauge(f"{platform}_t1", f"T1 in us", registry=registry)
+    t2 = Gauge(f"{platform}_t2", f"T2 in us", registry=registry)
+    fidelity = Gauge(f"{platform}_assignment_fidelity", f"T2 in us", registry=registry)
+    get_data(qibocal_output_folder)
