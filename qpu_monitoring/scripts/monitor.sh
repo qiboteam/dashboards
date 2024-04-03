66a5238df0cd81a8d81e037effe7bc18db9ce132
GabrielePalazzo <73099233+GabrielePalazzo@users.noreply.github.com>
2024-04-03 09:33:52 +0400

    fix: Using correct slurm partition

--- qpu_monitoring/scripts/monitor.sh
+++ qpu_monitoring/scripts/monitor.sh
@@ -12,18 +12,25 @@ then
 elif [[ $# == 1 ]];
 then
     echo preparing slurm script
+    slurm_partition=$1
 else
     echo too many arguments
     exit
 fi
 
 echo "#!/bin/bash" > monitoring_slurm_script
-echo "#SBATCH -p iqm5q" >> monitoring_slurm_script
+echo "#SBATCH -p ${slurm_partition}" >> monitoring_slurm_script
 echo "#SBATCH -o slurm.out" >> monitoring_slurm_script
 echo "" >> monitoring_slurm_script
 echo "qq auto ${runcard_path} -o ${report_path} -f" >> monitoring_slurm_script
 
-echo "python3 ${exporter_script_path}" >> metrics_export_slurm_script
+echo "python3 ${exporter_script_path}" > metrics_export_slurm_script
 
-monitoring_job_id=$(sbatch --parsable monitoring_slurm_script)
-sbatch --dependency=afterany:${monitoring_job_id} metrics_export_slurm_script
+# run both script with slurm
+# monitoring_job_id=$(sbatch --parsable monitoring_slurm_script)
+# sbatch --dependency=afterany:${monitoring_job_id} metrics_export_slurm_script
+
+# run the exporter locally
+sbatch -W --parsable monitoring_slurm_script
+wait
+python3 ${exporter_script_path}
