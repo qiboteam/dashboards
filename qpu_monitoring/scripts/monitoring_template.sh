eb67df4139cc4c4c3842db4e1f65b617c9851b72
GabrielePalazzo <73099233+GabrielePalazzo@users.noreply.github.com>
2024-04-04 13:10:19 +0400

    fix: Using correct slurm script

--- qpu_monitoring/scripts/monitoring_template.sh
+++ qpu_monitoring/scripts/monitoring_template.sh
@@ -3,7 +3,7 @@
 # run the exporter locally
 if [[ -z $(squeue -p {{ slurm_partition }} -u $USER -n monitoring_{{ platform }} | sed '1d') ]];
 then
-    sbatch -W --parsable monitoring_slurm_script
+    sbatch -W monitor.sh
     wait
 else
     echo "Another job is currently running for {{ platform }} on {{ slurm_partition }}"
