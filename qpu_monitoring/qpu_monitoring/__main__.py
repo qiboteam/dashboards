f030c3e876102a347747a068737fc571fbc2138c
GabrielePalazzo <73099233+GabrielePalazzo@users.noreply.github.com>
2024-04-04 13:39:22 +0400

    refactor: Reduced function arguments

--- qpu_monitoring/qpu_monitoring/__main__.py
+++ qpu_monitoring/qpu_monitoring/__main__.py
@@ -1,4 +1,5 @@
 import subprocess
+from dataclasses import dataclass
 from pathlib import Path
 
 from jinja2 import Environment, FileSystemLoader
@@ -11,17 +12,22 @@ REPORTS = Path.home() / "monitoring_reports"
 SLURM_JOBS = Path.home() / "monitoring_jobs"
 
 
+@dataclass
+class SlurmJobInfo:
+    partition: str
+    platform: str
+
+
 def generate_slurm_script(
-    slurm_partition: str,
-    qibolab_platform: str,
+    job_info: SlurmJobInfo,
     slurm_script_path: Path,
     report_save_path: Path,
 ):
     env = Environment(loader=FileSystemLoader(TEMPLATES))
     template = env.get_template("slurm_template.sh")
     slurm_monitoring_script = template.render(
-        slurm_partition=slurm_partition,
-        platform=qibolab_platform,
+        slurm_partition=job_info.partition,
+        platform=job_info.platform,
         runcard_path=RUNCARD,
         report_path=report_save_path,
     )
@@ -31,35 +37,31 @@ def generate_slurm_script(
 
 
 def generate_monitoring_script(
-    slurm_partition: str,
-    qibolab_platform: str,
+    job_info: SlurmJobInfo,
     slurm_script_path: Path,
 ):
     env = Environment(loader=FileSystemLoader(TEMPLATES))
     template = env.get_template("monitoring_template.sh")
     slurm_monitoring_script = template.render(
-        slurm_partition=slurm_partition,
-        platform=qibolab_platform,
+        slurm_partition=job_info.partition,
+        platform=job_info.platform,
     )
     slurm_script_path.parent.mkdir(parents=True, exist_ok=True)
     slurm_script_path.write_text(slurm_monitoring_script)
     slurm_script_path.chmod(0o744)
 
 
-def monitor_qpu(slurm_partition: str = None, qibolab_platform: str = "dummy"):
+def monitor_qpu(job_info: SlurmJobInfo):
     # generate slurm script
-    script_path = SLURM_JOBS / qibolab_platform
-    report_save_path = REPORTS / qibolab_platform
-    generate_slurm_script(
-        slurm_partition, qibolab_platform, script_path / "monitor.sh", report_save_path
-    )
+    script_path = SLURM_JOBS / job_info.platform
+    report_save_path = REPORTS / job_info.platform
+    generate_slurm_script(job_info, script_path / "monitor.sh", report_save_path)
     generate_monitoring_script(
-        slurm_partition,
-        qibolab_platform,
+        job_info,
         script_path / "slurm_qpu_monitor.sh",
     )
     # run acquisition job on slurm
-    if slurm_partition is None:
+    if job_info.partition is None:
         subprocess.run([script_path / "monitor.sh"])
     else:
         # use sbatch
@@ -70,10 +72,10 @@ def monitor_qpu(slurm_partition: str = None, qibolab_platform: str = "dummy"):
 
 def main():
     jobs = [
-        ("slurm_partition", "qibolab_platform"),
+        SlurmJobInfo("slurm_partition", "qibolab_platform"),
     ]
     for job in jobs:
-        monitor_qpu(*job)
+        monitor_qpu(job)
 
 
 if __name__ == "__main__":
