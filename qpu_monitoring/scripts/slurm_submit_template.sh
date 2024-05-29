eec76ea69e53c56bb311125a515603c6595b8eaa
Gabriele <73099233+GabrielePalazzo@users.noreply.github.com>
2024-05-29 07:27:46 +0200

    fix: Using correct qq auto bash script name

--- qpu_monitoring/scripts/slurm_submit_template.sh
+++ qpu_monitoring/scripts/slurm_submit_template.sh
@@ -3,7 +3,7 @@
 # run the exporter locally
 if [[ $(squeue -p {{ slurm_partition }} -u "$USER" -n monitoring_{{ platform }} | sed '1d') == "" ]];
 then
-    sbatch -W monitor.sh
+    sbatch -W qpu_monitor_template.sh
     wait
 else
     echo "Another job is currently running for {{ platform }} on {{ slurm_partition }}"
