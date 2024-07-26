f69519fcc82aee3462dd922e72abcf4fc74d1180
Gabriele Palazzo <73099233+GabrielePalazzo@users.noreply.github.com>
2024-07-26 22:16:08 +0200

    Using qibocal interfaces for reading results (#35)
    * build(deps): Using qibocal main
    
    * refactor: Read report metadata using qibocal's Output
    
    * refactor: Use qibocal Output object to read results
    
    * refactor: Code optimization and cleanup
    
    * docs: Added docstring for get_data
    
    * refactor: QpuData.qubit_metrics is now a dict[dict] with keys qubit_id and metric_name
    
    * docs: Updated docstring for QpuData.qubit_metrics
    
    * refactor: Removed explicit t1 path and deserialization
    
    * Apply suggestions from code review
    
    Co-authored-by: Alessandro Candido <candido.ale@gmail.com>
    
    ---------
    
    Co-authored-by: Alessandro Candido <candido.ale@gmail.com>

--- qpu_monitoring/qpu_monitoring/metrics_export.py
+++ qpu_monitoring/qpu_monitoring/metrics_export.py
@@ -1,72 +1,66 @@
 """Collect data from qibocal reports and upload them to prometheus."""
 
+import collections
 import datetime as dt
-import json
 from dataclasses import dataclass, field
 from pathlib import Path
 from typing import Any
 
 import yaml
 from prometheus_client import CollectorRegistry, Gauge, push_to_gateway
-from qibocal.auto.serialize import deserialize
+from qibocal.auto.output import Output
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
 from .database_schema import Base, Qubit
 
 
-def from_path(json_path: Path):
-    return json.loads(json_path.read_text())
-
-
 @dataclass
 class QpuData:
-    qubit_metrics: list[dict[str, Any]]
-    """List of metrics acquired using qibocal.
-    Its shape is equal to the number of quit of the platform.
-    Each element of the list contains a dictionary with acquired data."""
+    qubit_metrics: dict[str, dict[str, Any]]
+    """Dictionary of metrics acquired using qibocal.
+    Its outermost key is the qubit id. For each qubit, it stores
+    a dictionary with acquired data."""
     acquisition_time: dt.datetime = field(default_factory=dt.datetime.now)
     """Date and time of the qibocal acquisition."""
 
 
 def get_data(qibocal_output_folder: Path) -> QpuData:
-    qpu_data = []
-    path_t1 = deserialize(
-        from_path(qibocal_output_folder / "data" / "t1" / "results.json")
-    )
-    path_t2 = deserialize(
-        from_path(qibocal_output_folder / "data" / "t2" / "results.json")
-    )
-    path_fidelity = deserialize(
-        from_path(
-            qibocal_output_folder / "data" / "readout characterization" / "results.json"
-        )
-    )
-    # assuming all single qubit routines run on the same qubits
-    for qubit_id in path_t1["t1"]:
-        qubit_data = {
-            "t1": path_t1["t1"][qubit_id][0],
-            "t2": path_t2["t2"][qubit_id][0],
-            "assignment_fidelity": path_fidelity["assignment_fidelity"][qubit_id],
-        }
-        qpu_data.append(qubit_data)
-    report_meta = json.loads((qibocal_output_folder / "meta.json").read_text())
-    date = report_meta["date"]
-    time = report_meta["start-time"]
-    acquisition_time = dt.datetime.strptime(f"{date} {time}", "%Y-%m-%d %H:%M:%S")
+    """Read metrics acquired by qibocal from the report."""
+    out = Output.load(qibocal_output_folder)
+    report_meta = out.meta
+    acquisition_time = report_meta.start_time
+    qpu_data = collections.defaultdict(dict)
+
+    for task_id, result in out.history.items():
+        task_id = task_id.id
+        metric = task_id
+        if task_id == "readout characterization":
+            metric = "assignment_fidelity"
+
+        metric_values = getattr(result.results, metric)
+        for qubit_id, qubit_metric in metric_values.items():
+            if metric != "assignment_fidelity":
+                qubit_metric = qubit_metric[0]
+            qpu_data[qubit_id][metric] = qubit_metric
     return QpuData(qpu_data, acquisition_time)
 
 
 def push_data_prometheus(platform: str, qpu_data: QpuData):
     registry = CollectorRegistry()
     registry_gauges = {}
-    for key in qpu_data.qubit_metrics[0]:
-        gauge = Gauge(f"{platform}_{key}", f"{platform}_{key}", registry=registry)
-        registry_gauges[key] = gauge
+    for qubit_id, qubit_data in qpu_data.qubit_metrics.items():
+        for metric in qubit_data:
+            gauge = Gauge(
+                f"{platform}_{metric}_{qubit_id}",
+                f"{platform}_{metric}_{qubit_id}",
+                registry=registry,
+            )
+            registry_gauges[(qubit_id, metric)] = gauge
 
-    for qubit_data in qpu_data.qubit_metrics:
-        for key, value in qubit_data.items():
-            registry_gauges[key].set(value)
+    for qubit_id, qubit_data in qpu_data.qubit_metrics.items():
+        for metric_name, metric_value in qubit_data.items():
+            registry_gauges[(qubit_id, metric_name)].set(metric_value)
     push_to_gateway("localhost:9091", job="pushgateway", registry=registry)
 
 
@@ -84,17 +78,15 @@ def push_data_postgres(platform: str, qpu_data: QpuData, **kwargs):
     )
     Base.metadata.create_all(engine)
 
-    for i, qubit_data in enumerate(qpu_data.qubit_metrics):
+    for qubit_id, qubit_data in qpu_data.qubit_metrics.items():
         with Session(engine) as session:
             qubit = Qubit(
-                qubit_id=i,
+                qubit_id=qubit_id,
                 qpu_name=platform,
                 acquisition_time=qpu_data.acquisition_time,
                 **qubit_data,
             )
-
             session.add_all([qubit])
-
             session.commit()
 
 
