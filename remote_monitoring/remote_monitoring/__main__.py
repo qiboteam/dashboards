6fd2f8a32774d42707a21d6676935e991c1f4315
GabrielePalazzo <73099233+GabrielePalazzo@users.noreply.github.com>
2024-09-22 19:38:29 +0200

    feat: Run sbatch after ssh connection

--- remote_monitoring/remote_monitoring/__main__.py
+++ remote_monitoring/remote_monitoring/__main__.py
@@ -1,6 +1,10 @@
 import argparse
+import json
+import logging
 
 from .utils import key_based_connect
+
+logger = logging.getLogger(__name__)
 
 
 def main():
@@ -9,10 +13,27 @@ def main():
     parser.add_argument("--host", type=str, default=None)
     parser.add_argument("--username", type=str, default="qibocal")
     parser.add_argument("--private_key_password", type=str, default=None)
+    parser.add_argument("--slurm_configuration", type=str, default=None)
     args = parser.parse_args()
     client = key_based_connect(
         args.host, args.username, private_key_password=args.private_key_password
     )
+
+    slurm_configuration = json.loads(args.slurm_configuration)
+    _, _, sbatch_errors = client.exec_command(
+        "source .qpu_monitoring_env/bin/activate;"
+        "python -m qpu_monitoring --slurm_configuration "
+        f"'{json.dumps(slurm_configuration)}' --qibolab_platforms_path $HOME/qibolab_platforms_qrc"
+    )
+    logger.info(sbatch_errors.readlines())
+
+    for qpu in slurm_configuration:
+        _, command_line_output, _ = client.exec_command(
+            "cd monitoring_reports;"
+            f"cd {qpu['platform']};"
+            "OUTPUT_DIR=$(ls -t | head -1);"
+            "echo $OUTPUT_DIR"
+        )
 
 
 if __name__ == "__main__":
