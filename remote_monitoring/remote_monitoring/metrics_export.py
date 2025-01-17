c997bce34a536a17d04b495ed34edabe801fce4e
GabrielePalazzo <73099233+GabrielePalazzo@users.noreply.github.com>
2025-01-17 16:13:41 +0100

    refactor: Removed unused import

--- remote_monitoring/remote_monitoring/metrics_export.py
+++ remote_monitoring/remote_monitoring/metrics_export.py
@@ -7,7 +7,6 @@ from dataclasses import dataclass, field
 from pathlib import Path
 from typing import Any
 
-import yaml
 from qibocal.auto.output import Output
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
