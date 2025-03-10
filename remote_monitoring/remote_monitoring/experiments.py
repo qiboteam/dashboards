816342d52cd7fc4696ece820bf118482322a97b4
Gabriele Palazzo <73099233+GabrielePalazzo@users.noreply.github.com>
2025-03-10 09:47:24 +0100

    Define global constant with experiment-metric pairs (#48)
    * refactor: Define global constant with experiment-metric pairs
    
    * refactor: Use first element from result list if the metric is in the form [avg,err]
    
    * refactor: Move experiment metrics to separate file
    
    * docs: Updated instructions for adding new experiment metrics

---
+++ remote_monitoring/remote_monitoring/experiments.py
@@ -0,0 +1,8 @@
+"""Metrics to read from qibocal experiments."""
+
+METRICS = {
+    "t1": "t1",
+    "ramsey": "t2",
+    "readout_characterization": "assignment_fidelity",
+}
+"""Pair qibocal experiments with metrics to be displayed in grafana."""
