c64acdb85926fb83eab5bc9468e644e8d3e978ab
Gabriele <73099233+GabrielePalazzo@users.noreply.github.com>
2025-03-13 14:08:11 +0100

    feat: Implemented coherence-fidelity template in main

--- grafana/grafana_configuration/__main__.py
+++ grafana/grafana_configuration/__main__.py
@@ -1,11 +1,11 @@
 import argparse
-import dataclasses
 import json
 from pathlib import Path
 
-import jinja2
 from grafana_configuration import datasources
-from grafana_configuration.dashboards import Dashboard
+from grafana_configuration.templates.coherence_fidelity import (
+    CoherenceFidelityDashboard,
+)
 
 from .api_key import grafana_key
 from .users import change_admin_password, create_users
@@ -17,21 +17,6 @@ HTTP_HEADERS = {
     "Content-Type": "application/json",
 }
 GRAFANA_TEMPLATES_PATH = Path(__file__).parent / "templates"
-
-
-@dataclasses.dataclass
-class Metric:
-    name: str
-    color: str
-
-
-@dataclasses.dataclass
-class MetricPanel:
-    title: str
-    metrics: list[Metric]
-    width: int
-    height: int
-    unit: str
 
 
 def main():
@@ -51,42 +36,12 @@ def main():
     for data_source in data_sources:
         datasources.create(data_source, http_headers=HTTP_HEADERS)
 
-    coherence_metrics = [
-        Metric(name="t1", color="red"),
-        Metric(name="t2", color="blue"),
-    ]
-    fidelity_metrics = [
-        Metric(name="assignment_fidelity", color="green"),
-    ]
-    metric_panels = [
-        MetricPanel(
-            title="Coherence_times",
-            metrics=coherence_metrics,
-            width=5,
-            height=6,
-            unit="ns",
-        ),
-        MetricPanel(
-            title="Fidelities",
-            metrics=fidelity_metrics,
-            width=5,
-            height=6,
-            unit="percentunit",
-        ),
-    ]
-    env = jinja2.Environment(loader=jinja2.FileSystemLoader(GRAFANA_TEMPLATES_PATH))
-    template = env.get_template("coherence_fidelity.json")
     qpu_configuration = json.loads(args.qpu_config.read_text())
     for qpu_config in qpu_configuration["qpus"]:
-        json_string = template.render(
-            qpu_name=qpu_config["name"],
-            qubits=qpu_config["qubits"],
-            metric_panels=metric_panels,
+        dash = CoherenceFidelityDashboard(
+            title=qpu_config["name"], qubits=qpu_config["qubits"]
         )
-        dashboards_configurations = json.loads(json_string)
-        for dash_conf in dashboards_configurations:
-            dash = Dashboard.from_json_configuration(dash_conf)
-            dash.create(http_headers=HTTP_HEADERS)
+        dash.create(http_headers=HTTP_HEADERS)
 
     if args.users is not None:
         user_configurations = json.loads(args.users)
