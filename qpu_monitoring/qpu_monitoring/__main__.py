0901b8d1dc8fb7f2d9d5c2c7bff2cf0235ef169f
GabrielePalazzo <73099233+GabrielePalazzo@users.noreply.github.com>
2025-05-10 17:11:03 +0200

    fix: Made monitoring.py visible after having installed the package

--- qpu_monitoring/qpu_monitoring/__main__.py
+++ qpu_monitoring/qpu_monitoring/__main__.py
@@ -68,7 +68,7 @@ def generate_monitoring_script(
         targets=" ".join(job_info.targets),
         report_path=report_save_path,
         qibolab_platforms_path=job_info.qibolab_platforms_path,
-        monitoring_script_path=Path(__file__).parents[1] / "scripts" / "monitoring.py",
+        monitoring_script_path=Path(__file__).parent / "scripts" / "monitoring.py",
     )
     monitoring_script_path.parent.mkdir(parents=True, exist_ok=True)
     monitoring_script_path.write_text(monitoring_script)
