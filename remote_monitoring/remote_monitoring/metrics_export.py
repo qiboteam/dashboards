07f4feb8a41f64b1b9c3902cc828a35c35b79f51
Hisham Kazim <144886873+HishamKazim@users.noreply.github.com>
2025-07-18 16:38:48 +0400

    Update remote_monitoring/remote_monitoring/metrics_export.py
    Co-authored-by: Gabriele Palazzo <73099233+GabrielePalazzo@users.noreply.github.com>

--- remote_monitoring/remote_monitoring/metrics_export.py
+++ remote_monitoring/remote_monitoring/metrics_export.py
@@ -34,7 +34,6 @@ def get_data(qibocal_output_folder: Path) -> QpuData:
 
     for task_id, result in out.history.items():
         task_id = task_id.id
-        print("Task ID",task_id)
         metric = METRICS[task_id]
         metric_values = getattr(result.results, metric)
         for qubit_id, qubit_metric in metric_values.items():
